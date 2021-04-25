524ac354049902598e2e594f8cbcb780b05eecd2
xbw1220 <30739769+xbw1220@users.noreply.github.com>
2021-04-25 21:07:58 +0800

    update detail job

--- jobs/views.py
+++ jobs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 from jobs.models import Job
 from jobs.models import Cities, JobType
@@ -17,3 +17,12 @@ def joblist(request):
         job.city_name = Cities[job.job_city][1]
         job.job_type = JobType[job.job_type][1]
     return HttpResponse(template.render(context))
+
+
+def detail(request, job_id):
+    try:
+        job = Job.objects.get(pk=job_id)
+        job.city_name = Cities[job.job_city][1]
+    except Job.DoesNotExist:
+        raise Http404('Job deos not exist!')
+    return render(request, 'job.html', {'job': job})
